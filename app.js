2431157cdea17609d529b33d47137743e036a393
skirienkopanea <s.kirienkopanea@student.tudelft.nl>
2021-01-28 23:22:49 +0100

    big upload

--- app.js
+++ app.js
@@ -1,61 +1,27 @@
-//Variables for setup
+/******************************SET UP THE SERVER******************************/
 
-let container;
-let camera;
-let renderer;
-let scene;
-let house;
+//Import all packages (public) and modules (local)
+var express = require("express");
+var http = require("http");
 
-function init() {
-  container = document.querySelector(".scene");
+//Port config, create Express application an create server
+var port = process.argv[2];
+var app = express();
+var server = http.createServer(app);
 
-  //Create scene
-  scene = new THREE.Scene();
+//add middleware components
+//url logger
+app.use(function (request, response, next) {
+    console.log("%s\t%s\t%s\t%s\t", new Date(), request.ip.substr(7), request.method, request.url);
+    //for Heroku use request.headers['x-forwarded-for'] for the ip
+    next(); //control shifts to next middleware function (If we dont use this the user will be left hanging without a response)
+});
 
-  const fov = 35;
-  const aspect = container.clientWidth / container.clientHeight;
-  const near = 0.1;
-  const far = 1000;
+//this one feeds all the files requested that are nested in /public folder (this saves us from having to create request handlers for images, html files, audios etc.)
+app.use(express.static(__dirname + "/public"));
 
-  //Camera setup
-  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 5, 30);
-
-  const ambient = new THREE.AmbientLight(0x404040, 2);
-  scene.add(ambient);
-
-  const light = new THREE.DirectionalLight(0xffffff, 2);
-  light.position.set(50, 50, 100);
-  scene.add(light);
-  //Renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-  renderer.setSize(container.clientWidth, container.clientHeight);
-  renderer.setPixelRatio(window.devicePixelRatio);
-
-  container.appendChild(renderer.domElement);
-
-  //Load Model
-  let loader = new THREE.GLTFLoader();
-  loader.load("./house/scene.gltf", function(gltf) {
-    scene.add(gltf.scene);
-    house = gltf.scene.children[0];
-    animate();
+app.get('*', function (req, res) {
+    res.sendFile("/index.html", { root: "./public" });
   });
-}
 
-function animate() {
-  requestAnimationFrame(animate);
-  house.rotation.z += 0.005;
-  renderer.render(scene, camera);
-}
-
-init();
-
-function onWindowResize() {
-  camera.aspect = container.clientWidth / container.clientHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(container.clientWidth, container.clientHeight);
-}
-
-window.addEventListener("resize", onWindowResize);
+server.listen(process.env.PORT || port);
